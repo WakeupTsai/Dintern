0484e35c02df0170659f1fb93e2419f645aeb6c4
WakeupTsai <c20712983@gmail.com>
2015-07-09 17:33:47 +0800

    美化

--- ChatRoom/routes/index.js
+++ ChatRoom/routes/index.js
@@ -7,41 +7,5 @@ router.get('/', function(req, res, next) {
   res.render('chatroom-angular')
 });
 
-router.get('/chatApp', function(req, res, next) {
-  var content;
-  var filePath = path.join(__dirname, 'chatApp.js');
-
-  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err){
-    console.log('received data: ' + data);
-    response.writeHead(200, {'Content-Type': 'application/javascript'});
-    response.write(data);
-    response.end();
-    }else{
-        console.log(err);
-    }
-
-  });
-});
-
-
-router.get('/chatCtrl', function(req, res, next) {
-  var content;
-  var filePath = path.join(__dirname, 'chatCtrl.js');
-
-  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err){
-    console.log('received data: ' + data);
-    response.writeHead(200, {'Content-Type': 'application/javascript'});
-    response.write(data);
-    response.end();
-    }else{
-        console.log(err);
-    }
-
-  });
-
-});
-
 
 module.exports = router;
